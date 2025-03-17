b5064f8d34564e7cc9ab2d5fd3d1ac1d113881bd
Rootashish <42168072+Rootashish@users.noreply.github.com>
2025-03-17 12:49:05 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
-python -c "import nltk; nltk.download('stopwords')"
+python -c "import nltk; nltk.download('stopwords'); nltk.download('punkt')"
