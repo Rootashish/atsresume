db05c1160ac2e99630e4e73fa1f429e53f110905
Rootashish <42168072+Rootashish@users.noreply.github.com>
2025-03-17 12:18:12 +0530

    app.py

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def upload_resume():
     return jsonify({'error': 'Invalid file type'}), 400
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host='0.0.0.0', port=port)
